09127239e34c323bcbe64a5854d512718b15fd26
krishna71094 <kpsingh71094@gmail.com>
2018-12-02 07:22:01 +0530

    initial project

---
+++ pet-clinic-data/src/main/java/krishna/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package krishna.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ pet-clinic-data/src/main/java/krishna/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package krishna.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
